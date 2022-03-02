4eca8744eaf8da22a418bdc476c3a2eaaa3838f7
lumenwrites <lumenwrites@gmail.com>
2022-03-02 11:44:24 +0000

    Update.

--- components/Portfolio/Skills.tsx
+++ components/Portfolio/Skills.tsx
@@ -7,29 +7,27 @@ export default function Skills() {
         <div className="skill">
           <FontAwesomeIcon className="skill-icon" icon={['fas', 'laptop-code']} />
           <h2>Frontend</h2>
-          HTML/CSS/JS, TypeScript, React/Redux, jQuery, Sass, Bootstrap/Foundation, UI/UX Design in Figma.
+          React, Redux, TypeScript, jQuery, HTML, CSS, JS, Sass, Bootstrap, Foundation, UI/UX Design in Figma.
         </div>
         <div className="skill">
           <FontAwesomeIcon className="skill-icon" icon={['fas', 'server']} />
           <h2>Backend</h2>
-          Next.js, Node/Express, Python/Django, REST APIs, GraphQL, Apollo, Nexus, <br/>
+          Next.js, Node, Express, Python, Django, REST APIs, GraphQL, Apollo, Nexus, <br />
           Prisma, SQL, MongoDB, Postgres.
         </div>
         <div className="skill">
           <FontAwesomeIcon className="skill-icon" icon={['fas', 'tools']} />
           <h2>Tools</h2>
-          AWS, Vercel, Digital Ocean, Linux, <br/>
-          Git, Webpack, Docker, Nginx, <br/>
+          AWS, Vercel, Digital Ocean, Linux, <br />
+          Git, Webpack, Docker, Nginx, <br />
           Stripe, Sendgrid.
           {/* Jest/Enzyme. Python, Java, SQL. */}
         </div>
         <div className="skill">
           <FontAwesomeIcon className="skill-icon" icon={['fas', 'lightbulb']} />
           <h2>Other</h2>
-          2D and 3D Art and Graphic Design,
-          Technical Writing, Internet Marketing,
-          Machine Learning Libraries
-        {/* (scikit-learn, pandas, keras, etc.) */}
+          Technical and creative writing, teaching programming, internet marketing, SEO, 3D graphic design.
+          {/* Machine Learning Libraries (scikit-learn, pandas, keras, etc.) */}
           {/* Product management, Business skills */}
         </div>
       </div>
